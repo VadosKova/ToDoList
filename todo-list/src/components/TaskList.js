2429fc9cb2fd25baeadf4599523a2bad9dfcaca4
VadosKova <vadimkovalyov06@gmail.com>
2025-05-23 23:03:23 +0300

    Added function

--- todo-list/src/components/TaskList.js
+++ todo-list/src/components/TaskList.js
@@ -1,1 +1,17 @@
 import TaskItem from "./TaskItem";
+
+function TaskList({ tasks, onDelete, onToggle, onUpdate }) {
+  if (tasks.length === 0) {
+    return <p className="empty">No tasks yet 😴</p>;
+  }
+
+  return (
+    <div className="task-list">
+      {tasks.map((task) => (
+        <TaskItem key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} onUpdate={onUpdate}/>
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
