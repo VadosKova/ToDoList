ca7803094b394d95c41f24b6ec2c22c949f46ddc
VadosKova <vadimkovalyov06@gmail.com>
2025-05-23 18:01:46 +0300

    Updated main file

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+import TaskForm from "./components/TaskForm";
+import "./styles.css";
 
 function App() {
   return (
     <div className="app">
-      
+      <h1>📝 ToDo List</h1>
+      <TaskForm onAdd={addTask} />
     </div>
   );
 }
