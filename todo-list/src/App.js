fc6ca773d1713d0eff27ecfc7e96ff6f7d2118a4
VadosKova <vadimkovalyov06@gmail.com>
2025-05-23 23:18:20 +0300

    Added view

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,8 +1,9 @@
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
     <div className="app">
-      
+      <TaskList tasks={tasks} onDelete={deleteTask} onToggle={toggleComplete} onUpdate={updateTask}/>
     </div>
   );
 }
