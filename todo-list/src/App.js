68ca8df5d0d5bba67eaf7431de09699425751917
VadosKova <vadimkovalyov06@gmail.com>
2025-07-16 13:35:21 +0300

    Added functions for adding and deleting tasks

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -5,7 +5,15 @@ import logo from './logo.svg';
 import "./styles.css";
 
 function App() {
-  
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    setTasks([...tasks, { ...newTask, id: Date.now(), completed: false }]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 }
 
 export default App;
