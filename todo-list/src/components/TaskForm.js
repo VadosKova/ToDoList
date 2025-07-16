8ee8984e366dd5d7831b2ed4ef6e1959a95d091e
VadosKova <vadimkovalyov06@gmail.com>
2025-07-16 10:15:50 +0300

    Logic for sending data

--- todo-list/src/components/TaskForm.js
+++ todo-list/src/components/TaskForm.js
@@ -1,5 +1,15 @@
 import { useState } from "react";
 
 function TaskForm({ onAdd }) {
-    
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title.trim()) return;
+
+    onAdd({ title, description });
+    setTitle("");
+    setDescription("");
+  };
 }
