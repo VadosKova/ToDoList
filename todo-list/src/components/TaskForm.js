48795fb036eb7dca3e67cab9ec9937119ba4b243
VadosKova <vadimkovalyov06@gmail.com>
2025-07-16 10:18:57 +0300

    Added view for TaskForm

--- todo-list/src/components/TaskForm.js
+++ todo-list/src/components/TaskForm.js
@@ -12,4 +12,14 @@ function TaskForm({ onAdd }) {
     setTitle("");
     setDescription("");
   };
+
+  return (
+    <form className="task-form" onSubmit={handleSubmit}>
+      <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} required/>
+      <textarea placeholder="What you need to do?" value={description} onChange={(e) => setDescription(e.target.value)}/>
+      <button type="submit">Add task</button>
+    </form>
+  );
 }
+
+export default TaskForm;
