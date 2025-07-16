0f67699835b979e005dd38a615ed8ed723ee0a3c
VadosKova <vadimkovalyov06@gmail.com>
2025-07-16 10:44:05 +0300

    Logic for updating task

--- todo-list/src/components/TaskItem.js
+++ todo-list/src/components/TaskItem.js
@@ -4,5 +4,12 @@ function TaskItem({ task, onDelete, onToggle, onUpdate }) {
   const [isEditing, setIsEditing] = useState(false);
   const [editTitle, setEditTitle] = useState(task.title);
   const [editDescription, setEditDescription] = useState(task.description);
-
+  
+  function handleUpdate() {
+    onUpdate(task.id, {
+      title: editTitle,
+      description: editDescription,
+    });
+    setIsEditing(false);
+  }
 }
