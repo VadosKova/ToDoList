ee0e4360a214eb964eab3d7b07bf71507109a26a
VadosKova <vadimkovalyov06@gmail.com>
2025-07-16 10:42:42 +0300

    Added hook for condition

---
+++ todo-list/src/components/TaskItem.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+function TaskItem({ task, onDelete, onToggle, onUpdate }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editTitle, setEditTitle] = useState(task.title);
+  const [editDescription, setEditDescription] = useState(task.description);
+
+}
